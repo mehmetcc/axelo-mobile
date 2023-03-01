84329894278d0f27286c4ff86f0994f7a2bebcdd
mehmetcc <mehmetccm@gmail.com>
2023-03-01 21:42:29 +0300

    Deniz anan

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
+
 
 export default function App() {
+  const [started, setStarted] = useState(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textHeader}>Axelo</Text>
+      <TouchableOpacity
+        style={{
+          borderWidth: 1,
+          borderColor: 'rgba(0,0,0,0.2)',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: 100,
+          height: 100,
+          backgroundColor: '#fff',
+          borderRadius: 100,
+          backgroundColor: 'red'
+        }}
+        onPress={() => {
+          {
+            if (started) {
+              Accelerometer.removeAllListeners();
+              setStarted(false);
+            } else {
+              Accelerometer.addListener((x, y, z) => {
+                fetch('http://192.168.0.179:3000/acceleration', {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify([x, y, z])
+                })
+                  .then(response => console.log(response))
+                  .catch(error => console.error(error))
+              })
+            }
+          }
+        }
+        }
+      >
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +58,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textHeader: {
+    fontWeight: 'bold',
+    fontSize: 48
+  }
 });
---
+++ README.md
@@ -0,0 +1,13 @@
+# axelo-mobile
+
+## Running the project
+1. Make sure you have a running Expo installation on your phone and npm installed on your computer.
+2. Type in: 
+```shell
+npx expo start
+```
+3. After typing in the command, you will see a barcode on your terminal. 
+    * For iOS, scan that with the Camera app. That would launch the Expo app.
+    * For Android, open the Expo app, and scan the barcode from over that.
+
+You will see a brief interlude. That means that Expo is trying to bundle the Javascript source into an Electron executable. After a while, the app shall start and you should see logs of accelerometer data on your terminal.
